1081ba78be75a9f554e9c18cd65b129bb20bb837
Steven Shatz <sshatzdeveloper@gmail.com>
2024-05-14 15:09:45 -0400

    Show station table on the Home page

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import pandas as pd
 # Create Flask website instance
 app = Flask(__name__)
 
+port = 5002
+
+filename = "data_small/stations.txt"
+stations = pd.read_csv(filename, skiprows=17)
+
+stations = stations[['STAID', 'STANAME                                 ']]  # Station ID and Station Name
+# stations['URL'] = f"http://127.0.0.1:{port}/api/v1/{stations['STAID']}/1889-01-01"
+
+html_stations = stations.to_html()
+
 
 # Create Home page and grab tutorial.html
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=html_stations)
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -17,8 +27,8 @@ def about(station, date):
     # str(station).zfill(6)
     #
     # Here's another way using an f-string:
-    filename = "data_small/TG_STAID" + f"{station:0>6}" + ".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    txt_filename = "data_small/TG_STAID" + f"{station:0>6}" + ".txt"
+    df = pd.read_csv(txt_filename, skiprows=20, parse_dates=["    DATE"])
 
     # get temp on specified date
     df_row = df.loc[df['    DATE'] == date]
@@ -30,4 +40,4 @@ def about(station, date):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)  # port 5000 is the default
+    app.run(debug=True, port=port)  # port 5000 is the default
