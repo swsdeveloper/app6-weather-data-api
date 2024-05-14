29c0e079eddd776a69d7c40892a9cbc68738e5aa
Steven Shatz <sshatzdeveloper@gmail.com>
2024-05-13 23:32:53 -0400

    Return temperature for any station and date.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 # Create Flask website instance
 app = Flask(__name__)
@@ -12,10 +13,19 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    # df = pandas.csv("")
-    # temperature = df.station(date)
-    temperature = 23
-    result_dictionary = {"station": station, "date": date, "temperature": temperature}
+    # One way to convert station to a 6 byte string with leading zeros:
+    # str(station).zfill(6)
+    #
+    # Here's another way using an f-string:
+    filename = "data_small/TG_STAID" + f"{station:0>6}" + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    # get temp on specified date
+    df_row = df.loc[df['    DATE'] == date]
+    temperature = df_row['   TG'].squeeze() / 10  # celsius
+    fahrenheit = temperature * (9/5) + 32
+
+    result_dictionary = {"station": station, "date": date, "temperature": fahrenheit}
     return result_dictionary
 
 
